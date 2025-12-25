dfdcf4e858b2abc012a6f092989fc1318a7f2662
mujtabashaikh025 <mujtabashaikh025@gmail.com>
2025-12-25 15:12:12 +0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,11 @@ import streamlit as st
 
 #app_page = st.Page(page="app.py", title="📝 Document Verification")
 #compliance_page = st.Page(page="pages/compliance.py", title="🚀 Report Generation")
-st.image("nama-logo.png")
+col1,col2 = st.columns(2)
+with st.col1:
+    st.image("images/nama-logo.png")
+with st.col1:
+    st.image("images/velyana-logo.png")
 
 pages = {
     "Services": [
